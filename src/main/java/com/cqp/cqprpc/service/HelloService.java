b4e9384837b4579c353fe81fcc45f007964c92fb
iupikachu <2111053717@qq.com>
2021-11-16 10:02:35 +0800

    供rpc测试

---
+++ src/main/java/com/cqp/cqprpc/service/HelloService.java
@@ -0,0 +1,12 @@
+package com.cqp.cqprpc.service;
+
+/**
+ * @author cqp
+ * @version 1.0.0
+ * @ClassName HelloService.java
+ * @Description TODO
+ * @createTime 2021年11月05日 16:47:00
+ */
+public interface HelloService {
+    String sayHello(String name);
+}
