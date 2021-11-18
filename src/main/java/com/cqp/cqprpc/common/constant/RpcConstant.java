b22233ab2f4828981d7ce48f8d2206c00e7812f8
iupikachu <2111053717@qq.com>
2021-11-18 20:27:02 +0800

    常量类

---
+++ src/main/java/com/cqp/cqprpc/common/constant/RpcConstant.java
@@ -0,0 +1,29 @@
+package com.cqp.cqprpc.common.constant;
+
+/**
+ * @author cqp
+ * @version 1.0.0
+ * @ClassName RpcConstant.java
+ * @Description 定义rpc用到的常量
+ * @createTime 2021年11月18日 16:17:00
+ */
+public class RpcConstant {
+
+    public RpcConstant() {
+    }
+
+    /**
+     * Zookeeper服务注册地址
+     */
+    public static final String ZK_SERVICE_PATH = "/cqp-rpc";
+
+    /**
+     * 编码
+     */
+    public static final String UTF_8 = "UTF-8";
+
+    /**
+     * 路径分隔符
+     */
+    public static final String PATH_DELIMITER = "/";
+}
