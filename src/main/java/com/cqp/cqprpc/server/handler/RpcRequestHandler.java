248c32ffa7be300ffabb46dfb41e25162af6d873
iupikachu <2111053717@qq.com>
2021-11-23 13:48:19 +0800

    完善发送 RpcResponse 功能

--- src/main/java/com/cqp/cqprpc/server/handler/RpcRequestHandler.java
+++ src/main/java/com/cqp/cqprpc/server/handler/RpcRequestHandler.java
@@ -2,11 +2,18 @@ package com.cqp.cqprpc.server.handler;
 
 import com.cqp.cqprpc.message.RpcRequest;
 import com.cqp.cqprpc.message.RpcResponse;
+import com.cqp.cqprpc.server.register.ServiceObject;
+import com.cqp.cqprpc.server.register.ServiceRegister;
+import com.cqp.cqprpc.server.register.ZooKeeperRegister;
+import com.cqp.cqprpc.util.ApplicationContextUtils;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import lombok.extern.slf4j.Slf4j;
 
+
+import javax.annotation.Resource;
+import java.io.Serializable;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
@@ -21,6 +28,8 @@ import java.lang.reflect.Method;
 public class RpcRequestHandler extends SimpleChannelInboundHandler<RpcRequest> {
 
 
+
+
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, RpcRequest rpcRequest) throws Exception {
         RpcResponse rpcResponse = new RpcResponse();
@@ -31,15 +40,25 @@ public class RpcRequestHandler extends SimpleChannelInboundHandler<RpcRequest> {
         System.out.println("interfaceName:" + intefaceName);
         // 暂时模拟zookeeper的功能
         try {
-            if(intefaceName.equals("com.cqp.cqprpc.service.HelloService")){
-                String className = "com.cqp.cqprpc.service.HelloServiceImpl";
-                Class<?> clazz = Class.forName(className);
-                Object instance = clazz.newInstance();
-                Method method = clazz.getMethod(rpcRequest.getMethodName(), rpcRequest.getParameterTypes());
-                Object returnObject = method.invoke(instance, rpcRequest.getParameterValues());
-                rpcResponse.setReturnValue(returnObject);
-            }
+            ServiceRegister serviceRegister = (ServiceRegister) ApplicationContextUtils.getBean("serviceRegister");
+            ServiceObject serviceObject = serviceRegister.getServiceObject(intefaceName);
+            Object obj = serviceObject.getObj(); // serviceImpl对象
+            Class<?> clazz = obj.getClass(); // serviceImpl Class 对象
+            System.out.println(obj.getClass().getName());
+            Method method = clazz.getMethod(rpcRequest.getMethodName(), rpcRequest.getParameterTypes());
+            Object returnObject = method.invoke(obj, rpcRequest.getParameterValues());
+            rpcResponse.setReturnValue(returnObject);
+            rpcResponse.setReturnClassType(rpcRequest.getReturnType());
+//            if(intefaceName.equals("com.cqp.cqprpc.service.HelloService")){
+//                String className = "com.cqp.cqprpc.service.HelloServiceImpl";
+//                Class<?> clazz = Class.forName(className);
+//                Object instance = clazz.newInstance();
+//                Method method = clazz.getMethod(rpcRequest.getMethodName(), rpcRequest.getParameterTypes());
+//                Object returnObject = method.invoke(instance, rpcRequest.getParameterValues());
+//                rpcResponse.setReturnValue(returnObject);
+//            }
         } catch (Exception e) {
+            System.out.println(e.getMessage());
             throw new RuntimeException("远程调用出错!",e);
         }
 
